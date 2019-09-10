8c5b35982ecd7e395e090e075d8ef26741427b34
Petr Kadlec <petr.kadlec@shopsys.com>
2019-09-10 16:40:03 +0200

    fixup! fix extending base entity join with extended entity

--- packages/framework/src/Component/Doctrine/QueryBuilderExtender.php
+++ packages/framework/src/Component/Doctrine/QueryBuilderExtender.php
@@ -2,6 +2,7 @@
 
 namespace Shopsys\FrameworkBundle\Component\Doctrine;
 
+use BadMethodCallException;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
 use Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver;
@@ -11,16 +12,32 @@ class QueryBuilderExtender
     protected const REQUIRED_ALIASES_COUNT = 1;
 
     /**
-     * @var \Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver
+     * @var \Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver|null
      */
     protected $entityNameResolver;
 
     /**
-     * @param \Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver $entityNameResolver
+     * @param \Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver|null $entityNameResolver
      */
-    public function __construct(EntityNameResolver $entityNameResolver)
+    public function __construct(?EntityNameResolver $entityNameResolver = null)
     {
         $this->entityNameResolver = $entityNameResolver;
+    }
+
+    /**
+     * @required
+     * @internal This function will be replaced by constructor injection in next major
+     * @param \Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver $entityNameResolver
+     */
+    public function setEntityNameResolver(EntityNameResolver $entityNameResolver): void
+    {
+        if ($this->entityNameResolver !== null && $this->entityNameResolver !== $entityNameResolver) {
+            throw new BadMethodCallException(sprintf('Method "%s" has been already called and cannot be called multiple times.', __METHOD__));
+        }
+        if ($this->entityNameResolver === null) {
+            @trigger_error(sprintf('The %s() method is deprecated and will be removed in the next major. Use the constructor injection instead.', __METHOD__), E_USER_DEPRECATED);
+            $this->entityNameResolver = $entityNameResolver;
+        }
     }
 
     /**
